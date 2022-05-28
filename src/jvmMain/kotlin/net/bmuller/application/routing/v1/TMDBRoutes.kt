9ca9baaa49e5cf6625f872ab817ad3dc5577bb86
Bryan Müller <bmuller0975@gmail.com>
2022-05-28 00:43:09 -0500

    Feat/polymorphic deserialization (#12)
    * polymorphic deserialization for multi search types
    
    * common fields

--- src/jvmMain/kotlin/net/bmuller/application/routing/v1/TMDBRoutes.kt
+++ src/jvmMain/kotlin/net/bmuller/application/routing/v1/TMDBRoutes.kt
@@ -12,16 +12,23 @@ import net.bmuller.application.repository.TMDBRepository
 fun Route.tmdb() {
 	val tmdbRepository: TMDBRepository by inject()
 
-	get<TMDBResource.Search.Movies> { search ->
-		val searchTerm = search.searchTerm ?: ""
+	get<TMDBResource.Search.Movies> { context ->
+		val searchTerm = context.searchTerm ?: ""
 		tmdbRepository.searchMovies(searchTerm)
 			.mapLeft { error -> call.respond(HttpStatusCode.InternalServerError, error.toString()) }
 			.map { results -> call.respond(HttpStatusCode.OK, results) }
 	}
 
-	get<TMDBResource.Search.TV> { search ->
-		val searchTerm = search.searchTerm ?: ""
+	get<TMDBResource.Search.TV> { context ->
+		val searchTerm = context.searchTerm ?: ""
 		tmdbRepository.searchTVShows(searchTerm)
+			.mapLeft { error -> call.respond(HttpStatusCode.InternalServerError, error.toString()) }
+			.map { results -> call.respond(HttpStatusCode.OK, results) }
+	}
+
+	get<TMDBResource.Search.Multi> { context ->
+		val searchTerm = context.searchTerm ?: ""
+		tmdbRepository.searchMulti(searchTerm)
 			.mapLeft { error -> call.respond(HttpStatusCode.InternalServerError, error.toString()) }
 			.map { results -> call.respond(HttpStatusCode.OK, results) }
 	}
