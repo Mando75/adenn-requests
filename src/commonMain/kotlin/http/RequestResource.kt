e914f9d774e4f8d245120b1fa3ed12a4234712d7
Bryan Müller <bmuller0975@gmail.com>
2022-06-17 18:46:15 -0500

    implement request mutation client side

--- src/commonMain/kotlin/http/RequestResource.kt
+++ src/commonMain/kotlin/http/RequestResource.kt
@@ -3,6 +3,5 @@ package http
 import io.ktor.resources.*
 
 @kotlinx.serialization.Serializable
-@Resource(path = "/resources")
-class RequestResource {
-}
+@Resource("/requests")
+class RequestResource
--- src/jvmMain/kotlin/net/bmuller/application/routing/v1/RequestRoutes.kt
+++ src/jvmMain/kotlin/net/bmuller/application/routing/v1/RequestRoutes.kt
@@ -5,6 +5,7 @@ import http.RequestResource
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
+import io.ktor.server.resources.post
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import net.bmuller.application.plugins.inject
