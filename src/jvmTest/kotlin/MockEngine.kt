d2f8a6a6d4bb67f4fbd5ff2750c8f229a3bf1b95
Bryan Müller <bmuller0975@gmail.com>
2022-07-16 21:21:48 -0500

    setup fixtures for tmdb

--- src/jvmTest/kotlin/MockEngine.kt
+++ src/jvmTest/kotlin/MockEngine.kt
@@ -1,17 +1,15 @@
+import fixtures.*
 import io.ktor.client.engine.mock.*
 import io.ktor.http.*
 import io.ktor.utils.io.*
-import kotlinx.datetime.Clock
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import net.bmuller.application.entities.PlexAccountResponse
-import net.bmuller.application.entities.PlexCodeResponse
-import net.bmuller.application.entities.PlexFriendsResponse
-import net.bmuller.application.entities.PlexUser
+import net.bmuller.application.lib.GenericErrorModel
 
 val mockEngine = MockEngine { request ->
 	val (code, content) = when (request.url.host) {
 		"plex.tv" -> plexRouteHandler(request.url.encodedPath)
+		"api.themoviedb.org" -> tmdbRouteHandler(request.url.encodedPath)
 		else -> Pair(HttpStatusCode(418, "I'm a teapot"), """{"err": "Test Response"}""")
 	}
 
@@ -25,110 +23,29 @@ val mockEngine = MockEngine { request ->
 private fun plexRouteHandler(path: String): Pair<HttpStatusCode, String> = when (path) {
 	"/api/v2/pins" -> Pair(
 		HttpStatusCode.Created,
-		Json.encodeToString(
-			PlexCodeResponse(
-				authToken = "test-plex-auth-token",
-				clientIdentifier = "test-id",
-				code = "test-code",
-				createdAt = Clock.System.now().toString(),
-				expiresIn = 10000,
-				id = 1,
-				location = PlexCodeResponse.Location(
-					city = "New York",
-					code = "ZABD",
-					coordinates = "latlong",
-					country = "United States",
-					postalCode = "12345",
-					subdivisions = "",
-					timeZone = "EST"
-				),
-				newRegistration = false,
-				product = "PlexTV",
-				trusted = true,
-				expiresAt = ""
-			)
-		)
+		Json.encodeToString(plexCodeResponse)
 	)
 	"/api/v2/pins/1" -> Pair(
-		HttpStatusCode.OK, Json.encodeToString(
-			PlexCodeResponse(
-				authToken = "test-plex-auth-token",
-				clientIdentifier = "test-id",
-				code = "test-code",
-				createdAt = Clock.System.now().toString(),
-				expiresIn = 10000,
-				id = 1,
-				location = PlexCodeResponse.Location(
-					city = "New York",
-					code = "ZABD",
-					coordinates = "latlong",
-					country = "United States",
-					postalCode = "12345",
-					subdivisions = "",
-					timeZone = "EST"
-				),
-				newRegistration = false,
-				product = "PlexTV",
-				trusted = true,
-				expiresAt = ""
-			)
-		)
+		HttpStatusCode.OK, Json.encodeToString(plexCodeResponse)
 	)
 	"/users/account.json" -> Pair(
-		HttpStatusCode.OK, Json.encodeToString(
-			PlexAccountResponse(
-				user = PlexUser(
-					username = "testuser",
-					id = 1,
-					authToken = "plexauthtoken",
-					email = "test@bmuller.net",
-					uuid = "random-guid",
-					joinedAt = "",
-					title = "",
-					hasPassword = "",
-					subscription = PlexUser.Subscription(
-						active = true,
-						status = "active",
-						plan = "plexpass",
-						features = listOf("test-feature")
-					),
-					roles = PlexUser.Roles(roles = listOf("test-user")),
-					entitlements = listOf("test-entitlement"),
-					thumb = ""
-				)
-			)
-		)
+		HttpStatusCode.OK, Json.encodeToString(plexAccountResponse)
 	)
 	"/pms/friends/all" -> Pair(
-		HttpStatusCode.OK, Json.encodeToString(
-
-			PlexFriendsResponse(
-				friendlyName = "testplexserver",
-				identifier = "my-plex-machine-id",
-				machineIdentifier = "my-plex-machine-id",
-				totalSize = 1,
-				size = 1,
-				users = listOf(
-					PlexFriendsResponse.User(
-						id = 1,
-						title = "Test User",
-						username = "testuser",
-						email = "test@bmuller.net",
-						recommendationsPlaylistId = null,
-						thumb = "",
-						server = PlexFriendsResponse.Server(
-							id = 2,
-							serverId = 3,
-							machineIdentifier = "my-plex-machine-id",
-							name = "Test Server",
-							lastSeenAt = "",
-							numLibraries = 1,
-							owned = 1
-						)
-					)
-				)
-			)
-		)
+		HttpStatusCode.OK, Json.encodeToString(plexFriendsResponse)
 	)
-	else -> Pair(HttpStatusCode(418, "I'm a teapot"), """{"err": "invalid path"}""")
+	else -> Pair(
+		HttpStatusCode(418, "Missing Plex Route Mock"),
+		Json.encodeToString(GenericErrorModel("Missing Plex Path", path))
+	)
+}
+
+private fun tmdbRouteHandler(path: String): Pair<HttpStatusCode, String> = when (path) {
+	"/3/search/multi" -> Pair(HttpStatusCode.OK, searchMultiResponse)
+	"/3/search/movie" -> Pair(HttpStatusCode.OK, searchMovieResult)
+	"/3/search/tv" -> Pair(HttpStatusCode.OK, searchTVResult)
+	else -> Pair(
+		HttpStatusCode(418, "Missing TMDB Route Mock"),
+		Json.encodeToString(GenericErrorModel("Missing TMDB Route mock", path))
+	)
 }
